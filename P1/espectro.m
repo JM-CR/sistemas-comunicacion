075e2686ad58214fbc26b6651cdabd838563865a
Josue Mosh <msi_98_98@yahoo.com.mx>
2018-09-10 16:46:35 -0500

    Solución y documentación a P1

--- P1/espectro.m
+++ P1/espectro.m
@@ -1,7 +1,18 @@
-% @date 30/Agosto/2018
-% @author Josue Contreras
-% @author Rodrigo Arce
+% Calcula la CTFT de una función continua muestreada a frecuencia fs
+%       espectro(y, fs, xl, yl, tit)
+%
+% @param y Vector de valores muestreados
+% @param fs Frecuencia o tasa de muestreo
+% @param xl Etiqueta para el eje x
+% @param yl Etiqueta para el eje y
+% @param tit Título de la gráfica
+%
+% @author Josue M. Contreras Rocha
+% @author Rodrigo Roberto Arce Anguiano
+% @date 10/Septiembre/2018
+
 function espectro(y, fs, xl, yl, tit)
+
 % Está a 8000 muestras por segundo. Para sacar 100ms se necesita el 10%.
 rango = y(1:800);
 t = 0:1/fs:(length(rango) - 1)/fs;
@@ -15,21 +26,21 @@ subplot(211)
     legend('De 0 a 0.1 segundos')
     title('Señal de voz')
     
-% Limitar muestras de la senal y a 8000
-y = y(1:8000);
+% Limitar muestras de la senal y a fs
+y = y(1:fs);
     
 % Calcular la fft y centrarla
 val = fftshift(fft(y));
 
 % Crear vector de frecuencia
 f = -fs/2 : fs/2;
-f = f(1:8000);
+f = f(1:fs);
 
 % Graficar espectro
 subplot(212)
     plot(f, abs(val)); grid on
-    xlabel('Tiempo (s)')
-    ylabel('Amplitud')
-    title('Señal de voz')
-
+    xlabel(xl)
+    ylabel(yl)
+    title(tit)
+    
 end
