adb14d3e1c21a55ee990621f4ef0d6a80ca1a27a
Josue Mosh <msi_98_98@yahoo.com.mx>
2018-09-22 22:00:24 -0500

    Pasos de la solución - P2
    Se incluyen las imágenes con las que se trabajará en la práctica

---
+++ P2/compimage.m
@@ -0,0 +1,25 @@
+function compimage(dir, umbral)
+
+% Leer imagen original - 25/Septiembre/2018
+% Calcular probabilidad de cada mensaje - 25/Septiembre/2018
+% Dividir imagen en bloques de 8x8 pixeles
+% Aplicar DCT a cada bloque de 8x8 pixeles
+% Hacer cero los coeficientes de la DCT mayores a 'umbral'
+% Aplicar DCT inversa a la matriz con valores de cero
+
+% Calcular la entropía de la imagen original - 25/Septiembre/2018
+% Calcular la entropía de la DCT
+% Calcular la entropía de la DCT inversa
+% Calcular la entropía de la imagen recontruida
+% Calcular el error cuadrático medio (ECM)
+% Calcular potencia de la imagen
+% Calcular porcentaje de compresión
+% Calcular porcentaje Err (ECM/Pot)
+
+% Desplegar imagen original - 25/Septiembre/2018
+% Desplegar DCT
+% Desplegar DCT con coeficientes de cero
+% Desplegar imagen recreada
+% Desplegar los cálculos de ECM, Err y % de compresión
+
+end
