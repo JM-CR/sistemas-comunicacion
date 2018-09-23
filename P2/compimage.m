00d22875d94f011ea01493de66eacac7fd680fb3
Josue Mosh <msi_98_98@yahoo.com.mx>
2018-09-22 22:10:33 -0500

    Lectura y despliegue de imagen original - P2

--- P2/compimage.m
+++ P2/compimage.m
@@ -1,13 +1,20 @@
 function compimage(dir, umbral)
 
 % Leer imagen original - 25/Septiembre/2018
+A = imread(dir);
+I = rgb2gray(A);
+
 % Calcular probabilidad de cada mensaje - 25/Septiembre/2018
+
+
 % Dividir imagen en bloques de 8x8 pixeles
 % Aplicar DCT a cada bloque de 8x8 pixeles
 % Hacer cero los coeficientes de la DCT mayores a 'umbral'
 % Aplicar DCT inversa a la matriz con valores de cero
 
 % Calcular la entropía de la imagen original - 25/Septiembre/2018
+
+
 % Calcular la entropía de la DCT
 % Calcular la entropía de la DCT inversa
 % Calcular la entropía de la imagen recontruida
@@ -17,6 +24,11 @@ function compimage(dir, umbral)
 % Calcular porcentaje Err (ECM/Pot)
 
 % Desplegar imagen original - 25/Septiembre/2018
+H = figure
+subplot(221)
+    imshow(I)
+    title('Original')
+
 % Desplegar DCT
 % Desplegar DCT con coeficientes de cero
 % Desplegar imagen recreada
