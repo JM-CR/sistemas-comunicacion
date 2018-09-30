6bf653f21f9f722194653228cf72869232026c93
Josue Mosh <msi_98_98@yahoo.com.mx>
2018-09-29 19:38:12 -0500

    Despliegue de elementos adicionales - P2
    Se imprime el título, ECR, Err y porcentaje de compresión.

--- P2/compimage.m
+++ P2/compimage.m
@@ -35,7 +35,7 @@ cUmbral = C.*mCeros;
 fun2 = @(block_struct) idct2(block_struct.data);   % Función para cada bloque
 comprimida = blockproc(cUmbral,[8 8], fun2);
 
-% Calcular la entropía (H) de la imagen original - 25/Septiembre/2018
+% Calcular la entropía (H) de la imagen original
 H1 = entropia(I);
 
 % Calcular la entropía de la DCT
@@ -47,38 +47,49 @@ H3 = entropia(cUmbral);
 % Calcular la entropía de la imagen recontruida
 H4 = entropia(comprimida);
 
-% Calcular el error cuadrático medio (ECM) - Pendiente
-% Calcular potencia de la imagen - Moshi
-% Calcular porcentaje de compresión - Rodrigo
-% Calcular porcentaje Err (ECM/Pot) - Rodrigo
+% Calcular el error cuadrático medio (ECM)
+% Calcular potencia de la imagen
+% Calcular porcentaje de compresión
+noCeros = sum(abs(cUmbral(:)) > 0);
+pixeles = numel(cUmbral);
+porComp = (1 - (noCeros/pixeles))*100;
 
-% Desplegar imagen original - 25/Septiembre/2018
+% Calcular porcentaje Err (ECM/Pot)
+
+
+% Desplegar imagen original
 close all
 figure('units', 'normalized', 'outerposition', [0 0 1 1])
-
-P1 = subplot('Position', [0.07 0.5538 0.3347 0.3412]);
+T = annotation('textbox', [0.26 0.95 .45 .04]);     % Título global
+    set(T, 'String', ['Práctica 2 (Josue Contreras & Rodrigo Arce). Umbral de ' num2str(umbral)]);
+    set(T, 'FontWeight', 'Bold');
+    set(T, 'HorizontalAlignment', 'center');
+    set(T, 'LineStyle', 'none', 'FontSize', 15);
+    
+P1 = subplot('Position', [0.07 0.5238 0.3347 0.3412]);
     imshow(I)
     title('Original', 'FontSize', 14)
     text(P1.XLim(2)*1.05, P1.YLim(2)/2, ['H =  ',num2str(H1)], 'FontSize', 13)
 
 % Desplegar DCT
-P2 = subplot('Position', [0.5403 0.5538 0.3347 0.3412]);
+P2 = subplot('Position', [0.5403 0.5238 0.3347 0.3412]);
     imshow(C)
     title('DCT', 'FontSize', 14)
     text(P2.XLim(2)*1.05, P2.YLim(2)/2, ['H =  ',num2str(H2)], 'FontSize', 13)
 
-% Desplegar DCT con coeficientes de cero
+% Desplegar DCT con coeficientes de cero, ECM y potencia de imagen
 P3 = subplot('Position', [0.07 0.1100 0.3347 0.3412]);
     imshow(cUmbral)
     title('Umbral', 'FontSize', 14)
     text(P3.XLim(2)*1.05, P3.YLim(2)/2, ['H =  ',num2str(H3)], 'FontSize', 13)
-
-% Desplegar imagen recreada
+    text(P3.XLim(2)*.3, P3.YLim(2)*1.15, ['ECM =  ' num2str(porComp)], 'FontSize', 13)    % ECM
+    text(P3.XLim(2)*1.17, P3.YLim(2)*1.15, ['Err =  ' num2str(porComp)], 'FontSize', 13)   % Err
+    
+% Desplegar imagen recreada y porcentaje de compresión
 P4 = subplot('Position', [0.5403 0.1100 0.3347 0.3412]);
     imshow(uint8(comprimida))
     title('Comprimida', 'FontSize', 14)
     text(P4.XLim(2)*1.05, P4.YLim(2)/2, ['H =  ',num2str(H4)], 'FontSize', 13)
-
-% Desplegar los cálculos de ECM, Err y % de compresión - Moshi
-
+    text(P4.XLim(2)*.3, P4.YLim(2)*1.15, ['% Compresión =  ' num2str(porComp) '%'], 'FontSize', 13)   % Compresión
+    
 end
--- P2/test.m
+++ P2/test.m
@@ -5,62 +5,68 @@ color = imread(dir);
 
 % Convertir a BN y calcular entropia
 BN = rgb2gray(color);
-H1=entropy(BN);
-entropia(BN)
+H1=entropia(BN)
 
 % Aplicar DCT en bloques y calcular entropia
 fun = @(block_struct) dct2(block_struct.data);   % Función para cada bloque
 dct = blockproc(BN,[8 8], fun);
-H2=entropy(dct);
-entropia(dct)
+H2=entropia(dct)
 dctSin = dct;
 
 % Aplicar umbral y calcular entropia
 dct(abs(dct) < umbral) = 0;
-H3=entropy(dct);
-entropia(dct)
+H3=entropia(dct)
 dctCon = dct;
 
 % Aplicar IDCT en bloques y calcular entropia
 fun2 = @(block_struct) idct2(block_struct.data);   % Función para cada bloque
 comp = blockproc(dct,[8 8], fun2);
-H4=entropy(comp);
-entropia(comp)
+H4=entropia(comp);
 
 % Calcular el error cuadrático medio (ECM) - Pendiente
 % Calcular potencia de la imagen - Moshi
 % Calcular porcentaje de compresión - Rodrigo
-noCeros = sum(abs(dct(:)) > 0);
-pixeles = numel(dct);
+noCeros = sum(abs(comp(:)) > 0);
+pixeles = numel(comp);
 porComp = (1 - (noCeros/pixeles))*100;
 
 % Calcular porcentaje Err (ECM/Pot) - Rodrigo
 
 
 
-% Mostrar imagenes
+% Desplegar imagen original
 close all
 figure('units', 'normalized', 'outerposition', [0 0 1 1])
-
-P1 = subplot(221);
+T = annotation('textbox', [0.26 0.95 .45 .04]);     % Título global
+    set(T, 'String', ['Práctica 2 (Josue Contreras & Rodrigo Arce). Umbral de ', num2str(umbral)]);
+    set(T, 'FontWeight', 'Bold');
+    set(T, 'HorizontalAlignment', 'center');
+    set(T, 'LineStyle', 'none', 'FontSize', 15);
+    
+P1 = subplot('Position', [0.07 0.5238 0.3347 0.3412]);
     imshow(BN)
     title('Original', 'FontSize', 14)
     text(P1.XLim(2)*1.05, P1.YLim(2)/2, ['H =  ',num2str(H1)], 'FontSize', 13)
-    
-P2 = subplot(222);
+
+% Desplegar DCT
+P2 = subplot('Position', [0.5403 0.5238 0.3347 0.3412]);
     imshow(dctSin)
     title('DCT', 'FontSize', 14)
     text(P2.XLim(2)*1.05, P2.YLim(2)/2, ['H =  ',num2str(H2)], 'FontSize', 13)
 
-P3 = subplot(223);
+% Desplegar DCT con coeficientes de cero, ECM y potencia de imagen
+P3 = subplot('Position', [0.07 0.1100 0.3347 0.3412]);
     imshow(dctCon)
     title('Umbral', 'FontSize', 14)
     text(P3.XLim(2)*1.05, P3.YLim(2)/2, ['H =  ',num2str(H3)], 'FontSize', 13)
-
-P4 = subplot(224);
+    text(P3.XLim(2)*.3, P3.YLim(2)*1.15, ['ECM =  ' num2str(porComp)], 'FontSize', 13)    % ECM
+    text(P3.XLim(2)*1.17, P3.YLim(2)*1.15, ['Err =  ' num2str(porComp)], 'FontSize', 13)   % Err
+    
+% Desplegar imagen recreada y porcentaje de compresión
+P4 = subplot('Position', [0.5403 0.1100 0.3347 0.3412]);
     imshow(uint8(comp))
     title('Comprimida', 'FontSize', 14)
     text(P4.XLim(2)*1.05, P4.YLim(2)/2, ['H =  ',num2str(H4)], 'FontSize', 13)
-end
+    text(P4.XLim(2)*.3, P4.YLim(2)*1.15, ['% Compresión =  ' num2str(porComp) '%'], 'FontSize', 13)   % Compresión
 
-% Desplegar los cálculos de ECM, Err y % de compresión - Moshi
+end
