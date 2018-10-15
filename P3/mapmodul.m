0e1555312879f88b6e0041140e964e3544ad188d
Josue Mosh <msi_98_98@yahoo.com.mx>
2018-10-15 07:21:40 -0500

    Diseño de la solución - P3

---
+++ P3/mapmodul.m
@@ -0,0 +1,11 @@
+function c = mapmodul(v, modo, modul)
+
+% Escalar valores de 'v' a un rango de 0 a 255
+
+% Pasar cada valor a binario | En bytes
+
+% Generar bitstream
+
+% Mapear valores binarios a complejo
+
+end
---
+++ P3/ofdm.m
@@ -0,0 +1,8 @@
+function y = ofdm(arch, modo, modul)
+
+% Leer señal de audio
+
+% Mapeo de modulación
+c = mapmodul(v, modo, modul);
+
+end
